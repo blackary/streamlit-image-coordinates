e00f88a52df97d10781b3bc90e1190ef544932c4
Zachary Blackwood <zachary@streamlit.io>
2023-01-09 12:35:19 -0500

    Echo code

--- pages/dynamic_update.py
+++ pages/dynamic_update.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image, ImageDraw
+
 from streamlit_image_coordinates import streamlit_image_coordinates
 
 st.set_page_config(
@@ -16,29 +17,32 @@ if "points" not in st.session_state:
 
 "## Click on image"
 
-with Image.open("kitty.jpeg") as img:
-    draw = ImageDraw.Draw(img)
 
-    # Draw an ellipse at each coordinate in points
-    for point in st.session_state["points"]:
-        center = point
-        radius = 10
-        coords = (
-            center[0] - radius,
-            center[1] - radius,
-            center[0] + radius,
-            center[1] + radius,
-        )
-        draw.ellipse(coords, fill="red")
-
-    value = streamlit_image_coordinates(
-        img,
-        key="pil",
+def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
+    center = point
+    radius = 10
+    return (
+        center[0] - radius,
+        center[1] - radius,
+        center[0] + radius,
+        center[1] + radius,
     )
 
-    if value is not None:
-        point = value["x"], value["y"]
 
-        if point not in st.session_state["points"]:
-            st.session_state["points"].append(point)
-            st.experimental_rerun()
+with st.echo("below"):
+    with Image.open("kitty.jpeg") as img:
+        draw = ImageDraw.Draw(img)
+
+        # Draw an ellipse at each coordinate in points
+        for point in st.session_state["points"]:
+            coords = get_ellipse_coords(point)
+            draw.ellipse(coords, fill="red")
+
+        value = streamlit_image_coordinates(img, key="pil")
+
+        if value is not None:
+            point = value["x"], value["y"]
+
+            if point not in st.session_state["points"]:
+                st.session_state["points"].append(point)
+                st.experimental_rerun()
